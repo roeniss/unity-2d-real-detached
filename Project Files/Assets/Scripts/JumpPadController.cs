c73d8cc1af8f178f3b20e28cd7679bc7a75952b0
GrandNewStart <hbjw123@naver.com>
2020-04-25 10:43:48 +0900

    점프패드 애니메이션 구현

--- Project Files/Assets/Scripts/JumpPadController.cs
+++ Project Files/Assets/Scripts/JumpPadController.cs
@@ -6,7 +6,10 @@ public class JumpPadController : MonoBehaviour
 {
     public Rigidbody2D playerRigidbody;
     public GameObject playerCheck;
+    public GameObject jumpPad;
     public SwitchController switchController;
+    private Vector2 jumpPadPositionBefore;
+    private Vector2 jumpPadPositionAfter;
     public float playerCheckRadius;
     public float jumpPower;
     private bool playerOnPad;
@@ -14,6 +17,9 @@ public class JumpPadController : MonoBehaviour
 
     private void Start()
     {
+        jumpPadPositionBefore = jumpPad.transform.position;
+        jumpPadPositionAfter = jumpPadPositionBefore;
+        jumpPadPositionAfter.y += 1;
         activated = false;
     }
 
@@ -34,11 +40,15 @@ public class JumpPadController : MonoBehaviour
         {
             playerRigidbody.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             activated = true;
+            jumpPad.gameObject.transform.position = jumpPadPositionAfter;
             Invoke("Deactivate", 0.5f);
         }
     }
 
-    private void Deactivate() { activated = false; }
+    private void Deactivate() { 
+        activated = false;
+        jumpPad.gameObject.transform.position = jumpPadPositionBefore;
+    }
 
     private void OnDrawGizmos()
     {
